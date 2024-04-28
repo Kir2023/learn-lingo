78d1f6d01c97e68d75686baba458316ae200d847
Kir2023 <132088682+Kir2023@users.noreply.github.com>
2024-04-28 12:54:40 +0300

    create favorites page

---
+++ src/pages/FavoritesPage/FavoritesPage.jsx
@@ -0,0 +1,12 @@
+import { Container } from "../../globalStyles";
+import { Title } from "./FavoritesPage.styled";
+
+const FavoritesPage = () => {
+  return (
+    <Container>
+      <Title>You haven&apos;t added teachers to your favorites yet.</Title>
+    </Container>
+  );
+};
+
+export default FavoritesPage;
